5e8aba51e63de568af6226cb5ef64e8773aaa7b6
Matt Barbour <mbarbo000@gmail.com>
2025-02-13 12:57:12 -0800

    adding support for searching by ingredient

--- frontend/familyrecipes/recipeapp/views.py
+++ frontend/familyrecipes/recipeapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Recipes
+from .models import Recipes, Ingredients
 
 def recipe_list(request):
     recipes = Recipes.objects.all()
+    # Get all ingredients for the dropdown
+    ingredients = Ingredients.objects.all()
+    print(f"Found {ingredients.count()} ingredients")
+    print("Ingredients:", list(ingredients.values_list('name', flat=True)))
+    
     
     # Handle recipe name search
     recipe_query = request.GET.get('q')
@@ -12,6 +17,9 @@ def recipe_list(request):
     # Handle ingredient search
     ingredient_query = request.GET.get('ingredient')
     if ingredient_query:
-        recipes = recipes.filter(ingredients__name__icontains=ingredient_query)
+        recipes = recipes.filter(recipeingredients__ingredient__name__icontains=ingredient_query)
     
-    return render(request, 'recipe_list.html', {'recipes': recipes})
+    return render(request, 'recipe_list.html', {
+        'recipes': recipes,
+        'ingredients': ingredients,
+    })
