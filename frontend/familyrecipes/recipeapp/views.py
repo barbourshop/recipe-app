520f7d5373499f6af82a98c50ff8041affc9b361
Matt Barbour <mbarbo000@gmail.com>
2025-02-13 07:56:56 -0800

    updating style on results and adding search

--- frontend/familyrecipes/recipeapp/views.py
+++ frontend/familyrecipes/recipeapp/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from .models import Recipes
 
 def recipe_list(request):
-    recipeList = Recipes.objects.all()
-    return render(request, 'recipe_list.html', {'recipes': recipeList})
+    recipes = Recipes.objects.all()
+    
+    # Handle recipe name search
+    recipe_query = request.GET.get('q')
+    if recipe_query:
+        recipes = recipes.filter(title__icontains=recipe_query)
+    
+    # Handle ingredient search
+    ingredient_query = request.GET.get('ingredient')
+    if ingredient_query:
+        recipes = recipes.filter(ingredients__name__icontains=ingredient_query)
+    
+    return render(request, 'recipe_list.html', {'recipes': recipes})
